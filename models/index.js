af1cd0c73877fd5325cf2919b3a5788c37d30c19
VAIBHAV123456 <vaibhavdudhabhate10@gmail.com>
2024-10-04 12:22:09 +0530

    error resolve in models

--- models/index.js
+++ models/index.js
@@ -8,6 +8,8 @@ const sequelize = new Sequelize('First', 'qwt_root', 'Dbs@11**', {
 
   try {
     sequelize.authenticate();
+    sequelize.query("SET FOREIGN_KEY_CHECKS = 0");
+
     console.log('Connection has been established successfully.');
   } catch (error) {
     console.error('Unable to connect to the database:', error);
@@ -16,16 +18,20 @@ const sequelize = new Sequelize('First', 'qwt_root', 'Dbs@11**', {
   const db = {}
   db.sequelize = Sequelize;
   db.sequelize =sequelize;
-
   db.User =  require('./user')(sequelize,DataTypes,Model)
   db.Contact =  require('./contact')(sequelize,DataTypes,Model)
 
-  db.User.hasMany(db.Contact,{foreignKey:'user_id',as:"contact_details"})
-  db.Contact.belongsTo(db.User)
+  // db.User.hasOne(db.Contact,{foreignKey:'user_id',as:"contact_details"})
 
+  db.User.hasMany(db.Contact,{foreignKey:'user_id',as:"contact_details"})
+  db.Contact.belongsTo(db.User,{foreignKey:'user_id',as:"user_details"})
+  
   db.User.belongsToMany(db.Contact,{through:"user_contacts"})
   db.Contact.belongsToMany(db.User,{through:"user_contacts"})
   
-  db.sequelize.sync({force : false})
-
+    //  sequelize.query("SET FOREIGN_KEY_CHECKS = 0");
+     sequelize.sync({ force: true });
+    //  sequelize.query("SET FOREIGN_KEY_CHECKS = 1");
+     
+  
   module.exports = db;
